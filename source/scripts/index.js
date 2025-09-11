2d252067232d86a99446b02b2665f26f3433d1f6
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-09-11 20:12:49 +0300

    Дорабатывает JS в меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,14 @@
 /* в этот файл добавляет скрипты*/
+/* меню */
 const mainHeaderNavigation = document.querySelector('.main-header__navigation');
+/* кнопка меню */
 const mainHeaderButton = document.querySelector('.main-header__button');
 
+/* реализация меню без JS */
+mainHeaderNavigation.classList.remove('main-header__navigation--no-js');
+mainHeaderButton.classList.remove('main-header__button--no-js');
+
+/* открытие/закрытие меню */
 mainHeaderButton.addEventListener('click', () => {
   mainHeaderNavigation.classList.toggle('main-header__navigation--hidden');
   mainHeaderButton.classList.toggle('main-header__button--open');
