552e3eb56d4025896f12938943a742976cfd01f0
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-09-11 14:54:11 +0300

    Добавляет JS на кнопку меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+const mainHeaderNavigation = document.querySelector('.main-header__navigation');
+const mainHeaderButton = document.querySelector('.main-header__button');
+
+mainHeaderButton.addEventListener('click', () => {
+  mainHeaderNavigation.classList.toggle('main-header__navigation--hidden');
+});
