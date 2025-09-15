0132691ce561f10cabb4225bfe3c9201d99e914c
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-09-15 23:19:36 +0300

    Исправляет фон карты без JS и горизонтальный скролл

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,8 @@
 const mainHeaderNavigation = document.querySelector('.main-header__navigation');
 /* кнопка меню */
 const mainHeaderButton = document.querySelector('.main-header__button');
+/* карта-картинка */
+const cooperationMapBackground = document.querySelector('.cooperation__map-background');
 
 /* реализация меню без JS */
 mainHeaderNavigation.classList.remove('main-header__navigation--no-js');
@@ -13,3 +15,6 @@ mainHeaderButton.addEventListener('click', () => {
   mainHeaderNavigation.classList.toggle('main-header__navigation--hidden');
   mainHeaderButton.classList.toggle('main-header__button--open');
 });
+
+/* Реализация карты без JS */
+cooperationMapBackground.classList.remove('cooperation__map-background--no-js');
