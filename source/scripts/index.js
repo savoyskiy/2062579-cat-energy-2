e30e86986dc82e68465fcf3d0a0f949c06e62a7e
savoyskiy <78842859+savoyskiy@users.noreply.github.com>
2025-09-19 14:15:22 +0300

    Настраивает работу виджета карты без JS

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,8 +3,6 @@
 const mainHeaderNavigation = document.querySelector('.main-header__navigation');
 /* кнопка меню */
 const mainHeaderButton = document.querySelector('.main-header__button');
-/* карта-картинка */
-const cooperationMapBackground = document.querySelector('.cooperation__map-background');
 
 /* реализация меню без JS */
 mainHeaderNavigation.classList.remove('main-header__navigation--no-js');
@@ -15,6 +13,3 @@ mainHeaderButton.addEventListener('click', () => {
   mainHeaderNavigation.classList.toggle('main-header__navigation--hidden');
   mainHeaderButton.classList.toggle('main-header__button--open');
 });
-
-/* Реализация карты без JS */
-cooperationMapBackground.classList.remove('cooperation__map-background--no-js');
